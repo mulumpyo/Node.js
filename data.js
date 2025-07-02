bfde64720e43941454ad45afc93ee0c87615befe
admin <pc@pc.com>
2025-07-02 10:06:39 +0900

    first

---
+++ data.js
@@ -0,0 +1,9 @@
+const members = [
+  { id: "guest", name: "손님" },
+  { id: "user", name: "회원" },
+  { id: "admin", name: "관리자" },
+];
+
+let add = (num1, num2) => num1 + num2;
+
+module.exports = { members, add };
